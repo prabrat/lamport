cc3e708494b3800407e2d4862e0ab9b8c8cf6838
Ghoetken <ghoetken@terpmail.umd.edu>
2026-05-21 15:29:33 -0400

    small changes in lamport

--- src/lamport.rs
+++ src/lamport.rs
@@ -23,26 +23,23 @@ fn generate_keys(seed: &[u8; 16]) -> (Vec<[u8; 16]>, Vec<[u8; 16]>) {
 
 fn sign_message(msg: &[u8], sk: &Vec<[u8; 16]>) -> Vec<[u8; 16]> {
 
-    let msg_hash = Sha3_256::digest(msg); // hashing the msg inside 
+    let msg_hash: [u8; 16] = Sha3_256::digest(msg)[..16].try_into().unwrap(); // hashing the msg inside 
     let mut signature: Vec<[u8; 16]> = Vec::new();
 
-    for i in 0..128u32 { 
-        let msg_byte = i / 8;
-        let msg_bit = 7 - (i % 8);
+    for i in 0..128 { 
+        let byte = msg_hash[i / 8];
+        let bit = ((byte >> (7 - (i % 8))) & 1) as usize;
 
-        let bit_val = (msg_hash[msg_byte as usize] >> msg_bit) & 1; // Shifts curr bit to rightmost position and masks with 1 to isolate bit value
-        let sk_index = (i * 2 + bit_val as u32) as usize; // 2i if bit val is 0 else 2i + 1
-
-        signature.push(sk[sk_index]); // add to signature
+        signature.push(sk[i+bit]); // add to signature
     }
 
     signature 
     
 }
 
-fn verify_signature(message: &[u8], signature: &Vec<[u8; 16]>, public_key: &Vec<[u8; 16]>) -> bool {
+fn verify_signature(msg: &[u8], signature: &Vec<[u8; 16]>, public_key: &Vec<[u8; 16]>) -> bool {
     //compute message hash
-    let msg_hash: [u8; 16] = Sha3_256::digest(message)[..16].try_into().unwrap();
+    let msg_hash: [u8; 16] = Sha3_256::digest(msg)[..16].try_into().unwrap();
     //iterate through all bits
     for i in 0..128 {
         //find bit value (0 or 1)
